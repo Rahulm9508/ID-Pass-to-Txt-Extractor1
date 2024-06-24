becec3b3ef8e2c83bf66e1b4d6e7fca0a9ad65fb
Rahulm9508 <153660317+Rahulm9508@users.noreply.github.com>
2024-06-24 21:18:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+api_id =20346550
+api_hash = "bc79c3bea7a626887bdc0871eecf0327"
+bot_token = "6943343929:AAFuwiFaU09iJs82yfuGSNI3aLzK0gA8kd0"
+auth_users = "7491167754"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "7491167754"
 owner_users = [int(num) for num in osowner_users.split(",")]
